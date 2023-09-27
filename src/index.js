faa70471c0f90ae26616fcbfb911928e91a1d548
Galchony <122820542+Galchony@users.noreply.github.com>
2023-09-27 18:44:59 +0300

    Add express to project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Galia')
+const express = require('express');
+const app = express();
+
+const PORT = 6050;
+
+app.get('/', (req,res)=> {
+    res.send('Hello');
+})
+
+app.listen(PORT , ()=> console.log(`Server is running on port ${PORT}.`))
