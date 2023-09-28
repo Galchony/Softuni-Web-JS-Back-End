5001c9d5d15e812d439f0c8985d8838d2957e7f6
Galchony <122820542+Galchony@users.noreply.github.com>
2023-09-28 16:43:11 +0300

    Show cubes on home page

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,7 +1,9 @@
+const uniqid = require('uniqid');
 const cubes = [];
+
 exports.create = (cubeData) => {
     cubes.push({
-        id: cubes.length + 1,
+        id: uniqid(),
         ...cubeData,
     });
 }
