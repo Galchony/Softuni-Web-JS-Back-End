8cded9a85530ddab15230f655d669997b832a408
Galchony <122820542+Galchony@users.noreply.github.com>
2023-09-28 17:45:20 +0300

    Add details page

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,5 +1,13 @@
 const uniqid = require('uniqid');
-const cubes = [];
+const cubes = [
+    {
+        id: '34092740973094710',
+        name: 'Rubic',
+        description: 'For fun',
+        imageUrl: 'https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cq_auto:eco%2Cw_760/MTk3MDg5MjU5NDA3MDI1MjM1/rubik-cube-algorithms.webp',
+        difficultyLevel: 5
+    }
+];
 
 exports.create = (cubeData) => {
     cubes.push({
@@ -8,3 +16,4 @@ exports.create = (cubeData) => {
     });
 }
 exports.getAll = () => cubes.slice();
+exports.getOne = (cubeId) => cubes.find(x=> x.id == cubeId);
