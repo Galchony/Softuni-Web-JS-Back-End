4179fe2b116e3e8d8c79899c68a516d7d7f9b47a
Galchony <122820542+Galchony@users.noreply.github.com>
2023-09-28 18:07:17 +0300

    Add search

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -15,5 +15,20 @@ exports.create = (cubeData) => {
         ...cubeData,
     });
 }
-exports.getAll = () => cubes.slice();
-exports.getOne = (cubeId) => cubes.find(x=> x.id == cubeId);
+exports.getAll = (search, from, to) => {
+    let result = cubes.slice();
+
+    if (search) {
+        result = result.filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
+    }
+    if (from) {
+        result = result.filter(x => x.difficultyLevel >= Number(from))
+    }
+    if (to) {
+        result = result.filter(x => x.difficultyLevel <= Number(to))
+    }
+    return result;
+
+}
+
+exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
