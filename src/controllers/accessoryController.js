72dcc6c11126286c92a8203933b2d0c10e762a01
Galia Angelova <po6tata_na_galia@abv.bg>
2023-09-30 17:57:29 +0300

    Add accessory manager

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -3,5 +3,10 @@ const router = require("express").Router();
 router.get("/create", (req, res) => {
   res.render("accessory/createAccessory");
 });
+router.post("/create", (req, res) => {
+
+    
+  res.redirect("/");
+});
 
 module.exports = router;
--- src/models/Cube.js
+++ src/models/Cube.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const cubeSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    imageUrl: String,
-    difficultyLevel: Number,
-})
+  name: String,
+  description: String,
+  imageUrl: String,
+  difficultyLevel: Number,
+});
 
-const Cube = mongoose.model('Cube', cubeSchema);
+const Cube = mongoose.model("Cube", cubeSchema);
 
-module.exports =  Cube;
+module.exports = Cube;
