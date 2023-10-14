34c43353376eefec86995d4082cea052058a930e
Galia Angelova <po6tata_na_galia@abv.bg>
2023-10-14 16:00:47 +0300

    Hash password before save

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
+const userManager = require("../managers/userManager");
 
 router.get("/register", async (req, res) => {
- 
-    res.render("register");
-  });
+  res.render("register");
+});
+router.post("/register", async (req, res) => {
+  const { username, password, repeatPassword } = req.body;
+  userManager.register({ username, password, repeatPassword });
+  res.redirect("/users/login");
+});
 
 module.exports = router;
